a56eceec26065d52bdf17d61f23583f04110d6c0
Donghwa Shin <dhshin0301@gmail.com>
2025-07-27 13:35:06 -0400

    Refactored slurm file for clarity and organization

--- QTracker_prod/scripts/train.sh
+++ QTracker_prod/scripts/train.sh
@@ -1,14 +1,21 @@
+# If using Rivanna, train using train.slurm instead
+
 LEARNING_RATE=0.00005
 
-python training_scripts/TrackFinder_unet.py \
- data/processed_files/mc_events.root \
- --output_model models/track_finder_unet.h5 \
+python models/TrackFinder_unet.py \
+ data/processed_files/mc_events_train.root \
+ data/processed_files/mc_events_val.root \
+ --output_model checkpoints/track_finder_unet.h5 \
  --learning_rate $LEARNING_RATE \
  --batch_norm 1 \
  --dropout_bn 0.5 \
  --dropout_enc 0.4 \
  --backbone None
 
-# python evaluate.py \
-#  data/processed_files/mc_events.root \
-#  models/track_finder_unet.h5
+python evaluate.py \
+ data/processed_files/mc_events_val.root \
+ checkpoints/track_finder_unet.h5
+
+python evaluate.py \
+ data/processed_files/mc_events_val_high.root \
+ checkpoints/track_finder_unet.h5
