89d806bf938f922a83d67f482272d0ee5536f1bc
Donghwa Shin <dhshin0301@gmail.com>
2025-08-21 23:38:15 +0900

    Added better deep supervision

--- QTracker_prod/evaluate.py
+++ QTracker_prod/evaluate.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 # core TrackFinder loaders / custom loss
 from models import data_loader
-from models import TrackFinder_unetpp
+from models import TrackFinder_unetpp_ds
 from models.losses import custom_loss
 import QTracker_prod
 from refine import refine_hit_arrays
@@ -59,16 +59,17 @@ def evaluate_model(root_file, model_path, use_bn=False, base=64, deep_supervisio
     }
     with tf.keras.utils.custom_object_scope(custom_objects):
         if ".weights.h5" in model_path:
-            model = TrackFinder_unetpp.build_model(
+            model = TrackFinder_unetpp_ds.build_model(
                 use_bn=use_bn, 
                 base=base,
-                deep_supervision=deep_supervision
             )
             model.load_weights(model_path)
         else:
             model = tf.keras.models.load_model(model_path)
 
     y_pred = model.predict(X_test)  # shape: (num_events, 2, num_detectors, num_elementids)
+    if ".weights.h5" in model_path:
+        y_pred = y_pred[0]
 
     softmax_mup = y_pred[:, 0, :, :]   # (num_events, num_detectors, num_elementids)
     softmax_mum = y_pred[:, 1, :, :]   # (num_events, num_detectors, num_elementids)
